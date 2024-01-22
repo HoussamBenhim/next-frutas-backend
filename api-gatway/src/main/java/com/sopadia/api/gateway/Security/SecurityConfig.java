c5b8b7ac6350202aa074add807f314c90c542569
hbenhim <benhim.houssam@gmail.com>
2024-01-22 17:26:03 +0100

    security

--- api-gatway/src/main/java/com/sopadia/api/gateway/Security/SecurityConfig.java
+++ api-gatway/src/main/java/com/sopadia/api/gateway/Security/SecurityConfig.java
@@ -1,34 +1,38 @@
 package com.sopadia.api.gateway.Security;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 
 @Configuration
-@EnableWebSecurity
-@EnableMethodSecurity
+@EnableWebFluxSecurity
 public class SecurityConfig {
-    private final JwtConverter jwtConverter;
-
-    public SecurityConfig(JwtConverter jwtConverter) {
-        this.jwtConverter = jwtConverter;
-    }
+   @Value("${spring.security.oauth2.resourceserver.jwt.jwk-set-uri}")
+   String jwkSetUri;
 
     @Bean
-    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity httpSecurity){
-         return httpSecurity.csrf(csrfSpec -> csrfSpec.disable())
-
-                 .authorizeExchange(exchange->exchange
-                         .pathMatchers(HttpMethod.GET).permitAll()
-
-                         .pathMatchers("/eureka/**","/api/products/**").permitAll()
-                        // .pathMatchers("/api/products/**").hasRole("dev")
-                         .anyExchange().authenticated())
-                 .oauth2ResourceServer(oauth-> oauth.jwt(jwtSpec -> jwtSpec.jwtAuthenticationConverter(jwtConverter))).build();
+    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity httpSecurity) {
+        return httpSecurity
+                .csrf(csrfSpec -> csrfSpec.disable())
+                .authorizeExchange(exchange -> exchange
+                        .pathMatchers(HttpMethod.GET).permitAll()
+                        .pathMatchers("/eureka/**").permitAll()
+                        .pathMatchers("/api/products/**").authenticated()
+                        .anyExchange().authenticated())
+                //.oauth2ResourceServer(oauth-> oauth.jwt(jwtSpec -> jwtSpec.jwtAuthenticationConverter(jwtConverter))).build();
+                .oauth2ResourceServer(oauth -> oauth.jwt(Customizer.withDefaults())).build();
     }
+
+   @Bean
+    JwtDecoder jwtDecoder(){
+        return NimbusJwtDecoder.withJwkSetUri(this.jwkSetUri).build();
+   }
 }
